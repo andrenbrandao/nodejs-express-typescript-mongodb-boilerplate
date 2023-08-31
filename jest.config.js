120d6aafda67f4e3c9ffdd164c1a8e62a4c5f532
André Brandão <andrenbrandao@gmail.com>
2023-08-31 19:48:06 -0300

    chore: install jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  clearMocks: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+};
---
+++ src/index.spec.ts
@@ -0,0 +1,5 @@
+describe('test', () => {
+  it('should sum a + b', () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
